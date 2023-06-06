52231d14ef52ad6cad062cf9ec5fafbe1c4747b5
Original-pokemon <eremkin-02@mail.ru>
2023-06-06 16:02:20 +0300

    refactor: del events-item

--- src/view/events-item-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../framework/view/abstract-stateful-view';
-
-const createEventsItemTemplate = () => '<li class="trip-events__item"> </li>';
-
-export default class EventsItemView extends AbstractView {
-  get template() {
-    return createEventsItemTemplate();
-  }
-}
